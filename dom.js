8f97ab06dc892998b1b3b323dfdc42669967f12b
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-10-07 11:06:17 +0100

    fixed linters errors

--- dom.js
+++ dom.js
@@ -11,19 +11,17 @@ let sign = null;
 const index = (el) => [...el.parentElement.children].indexOf(el);
 
 const Player = (name) => {
-    
-const getName = () => name;
-return {getName};
-
+  const getName = () => name;
+  return { getName };
 };
 
 const gameBoard = (() => {
-    const newArray = ['','','','','','','','',''];
-    let gameArray = ['','','','','','','','',''];
-    let gameOver = false;
+  const newArray = ['', '', '', '', '', '', '', '', ''];
+  let gameArray = ['', '', '', '', '', '', '', '', ''];
+  const gameOver = false;
 
 
-    const win = () => {
+  const win = () => {
     cont.innerHTML = `
   <h1 class="head" id="winner"> </h1>
     <div class="status_reset">
@@ -46,22 +44,19 @@ const gameBoard = (() => {
    
   </div>
   `;
-
   };
-  
 
 
-      const checkWin = () => {
-    
-const topLEft = gameArray[0];
-const topMiddle = gameArray[1];
-const topRight = gameArray[2];
-const middleLeft = gameArray[3];
-const middleMiddle = gameArray[4];
-const middleRight =gameArray[5];
-const botomleft = gameArray[6];
-const botomMiddle =  gameArray[7];
-const botomRight = gameArray[8];
+  const checkWin = () => {
+    const topLEft = gameArray[0];
+    const topMiddle = gameArray[1];
+    const topRight = gameArray[2];
+    const middleLeft = gameArray[3];
+    const middleMiddle = gameArray[4];
+    const middleRight = gameArray[5];
+    const botomleft = gameArray[6];
+    const botomMiddle = gameArray[7];
+    const botomRight = gameArray[8];
 
     if ((topLEft && topLEft === topMiddle && topLEft === topRight) || (topLEft && topLEft === middleLeft && topLEft === botomleft) || (topLEft && topLEft === middleMiddle && topLEft === botomRight)) {
       sign = topLEft;
@@ -103,58 +98,48 @@ const botomRight = gameArray[8];
   };
 
 
-   const next = () => {
+  const next = () => {
     const round = document.getElementById('play');
     if (isX) {
       round.innerHTML = `${playerOne.getName()} is next`;
-    } 
-
-    else if (isX===null){
-      if (sign==='x'){
-              round.innerHTML = `${playerOne.getName()} WON`;
-      isX = true;
+    } else if (isX === null) {
+      if (sign === 'x') {
+        round.innerHTML = `${playerOne.getName()} WON`;
+        isX = true;
+      } else {
+        round.innerHTML = `${playerTwo.getName()} WON`;
+        isX = true;
       }
-            else{
-              round.innerHTML = `${playerTwo.getName()} WON`;
-      isX = true;
-      }
-
-    }
-    else {
+    } else {
       round.innerHTML = `${playerTwo.getName()} is next`;
     }
   };
 
 
-
-
-  
-   const change = (e) => {
+  const change = (e) => {
     const allClass = e.target.classList;
     console.log('inside');
 
     if (allClass[3] !== 'x' && allClass[3] !== 't') {
       if (isX === true) {
         isX = false;
-        let ind = index(e.target);
+        const ind = index(e.target);
         e.target.classList.add('x');
-        gameArray[ind]='X'
+        gameArray[ind] = 'X';
         renderBoard();
         checkWin();
         next();
       } else {
         isX = true;
-        let ind = index(e.target);
+        const ind = index(e.target);
         e.target.classList.add('t');
-        gameArray[ind]='0'
+        gameArray[ind] = '0';
         renderBoard();
         checkWin();
         next();
       }
     }
   };
-
-
 
 
   const clickEvent = () => {
@@ -165,66 +150,46 @@ const botomRight = gameArray[8];
 
 
   const renderBoard = () => {
-      let i=0;
-    for (const cell of cellBloc ) {
-      cell.innerHTML= gameArray[i];
+    let i = 0;
+    for (const cell of cellBloc) {
+      cell.innerHTML = gameArray[i];
       i++;
     }
   };
 
   const reset = () => {
-    gameArray=['','','','','','','','',''];
+    gameArray = ['', '', '', '', '', '', '', '', ''];
     renderBoard();
     clickEvent();
     next();
-  }
+  };
 
 
-
-    return {
-        reset, clickEvent, renderBoard, next
-    }
+  return {
+    reset, clickEvent, renderBoard, next,
+  };
 })();
 
 
-
-
-
-
-
-
-
-
-
-
-
 const gameController = (() => {
+  const createPlayers = () => {
+    const nameOne = document.getElementById('form3').value;
+    const nameTwo = document.getElementById('form2').value;
+    if ((nameOne !== '') && (nameTwo !== '')) {
+      playerOne = Player(nameOne);
+      playerTwo = Player(nameTwo);
+      round.innerHTML = '';
+      diplayBoard();
+      gameBoard.renderBoard();
+      gameBoard.clickEvent();
+      gameBoard.next();
+    } else {
+      round.innerHTML = 'ENTER VALID NAMES';
+    }
+  };
 
-
-
-
-
-const createPlayers = () => {
-let nameOne = document.getElementById('form3').value;
-let nameTwo = document.getElementById('form2').value;
-if ((nameOne !=='') && (nameTwo !=='')) {
- playerOne = Player(nameOne);
- playerTwo = Player(nameTwo);
- round.innerHTML =''
- diplayBoard();
-gameBoard.renderBoard();
-gameBoard.clickEvent();
-gameBoard.next();
-}
-else {
-  round.innerHTML ='ENTER VALID NAMES'
-}
-
-
-};
-
-const diplayBoard = () => {
-      cont.innerHTML = `
+  const diplayBoard = () => {
+    cont.innerHTML = `
   <h1 class="head" id="winner"> </h1>
     <div class="status_reset">
     <div class="status"> <span id="play">  </span></div>
@@ -245,88 +210,75 @@ const diplayBoard = () => {
    
   </div>
   `;
-  }
-  
+  };
 
 
-
-    return {
-        createPlayers
-    }
-
+  return {
+    createPlayers,
+  };
 })();
 
 
 btnNext.addEventListener('click', gameController.createPlayers);
 
 
-
-
-
-
-  // const validateForm = () => {
-  //   const name = document.getElementById('uname').value;
-  //   const author = document.getElementById('uname2').value;
-  //   if (name === '' || author === '') {
-  //     return false;
-  //   }
-  //   return true;
-  // };
-  
-
-
-
+// const validateForm = () => {
+//   const name = document.getElementById('uname').value;
+//   const author = document.getElementById('uname2').value;
+//   if (name === '' || author === '') {
+//     return false;
+//   }
+//   return true;
+// };
 
 
 // const display = (() => {
 
-  
 
+// const welcome = () => {
+//   cont.innerHTML = `
+// <h1 class="head">TIC TAC TOE JS</h1>
+// <div class="was-validated form_div">
+//   <div class="form-group ">
+//     <label for="uname">PLAYER 1:</label>
+//     <input type="text" class="form-control" id="uname" placeholder="NAME PLAYER1" name="uname" required>
+//   </div>
+//   <div class="form-group ">
+//     <label for="uname">PLAYER 2:</label>
+//     <input type="text" class="form-control" id="uname2" placeholder="NAME PLAYER2" name="uname" required>
+//   </div>
+//   <button type="submit" class="btn btn-primary" onclick="createplayers()">Submit</button>
+// </div>
+// <div class=" gameboard">
+//   <div class="grid-game">
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+//  <div class="cell"> </div>
+// </div>
 
-  // const welcome = () => {
-  //   cont.innerHTML = `  
-  // <h1 class="head">TIC TAC TOE JS</h1>
-  // <div class="was-validated form_div">
-  //   <div class="form-group ">
-  //     <label for="uname">PLAYER 1:</label>
-  //     <input type="text" class="form-control" id="uname" placeholder="NAME PLAYER1" name="uname" required>
-  //   </div>
-  //   <div class="form-group ">
-  //     <label for="uname">PLAYER 2:</label>
-  //     <input type="text" class="form-control" id="uname2" placeholder="NAME PLAYER2" name="uname" required>
-  //   </div>
-  //   <button type="submit" class="btn btn-primary" onclick="createplayers()">Submit</button>
-  // </div>
-  // <div class=" gameboard">
-  //   <div class="grid-game"> 
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  //  <div class="cell"> </div>
-  // </div>
-   
-  // </div>
-  // `;
-  // };
+// </div>
+// `;
+// };
 
 
 //   const chooseSign = () => {
 //     if (validateForm()) {
 //       cont.innerHTML = `
 //    <h1 class="head">TIC TAC TOE JS</h1>
-//   <div class="form_div"> 
+//   <div class="form_div">
 //   <h4 class="player"> Welcome, ${playerOne.getName()} your sign is: X<h4>
 //   <h4 class="player"> Welcome, ${playerTwo.getName()} your sign is: 0<h4>
 //   <button type="submit" class="btn btn-primary" onclick="display.renderBoard()">NEXT</button>
 //   <h6>Click to start the game<h6>
 //   </div>
 //   <div class=" gameboard">
-//    <div class="grid-game"> 
+//    <div class="grid-game">
 //    <div class="cell"> </div>
 //    <div class="cell"> </div>
 //    <div class="cell"> </div>
@@ -337,8 +289,8 @@ btnNext.addEventListener('click', gameController.createPlayers);
 //    <div class="cell"> </div>
 //    <div class="cell"> </div>
 //   </div>
-   
-//   </div>  
+
+//   </div>
 //   `;
 //     } else {
 //       welcome();
@@ -346,11 +298,8 @@ btnNext.addEventListener('click', gameController.createPlayers);
 //   };
 
 
-
-
 //   return { welcome, chooseSign, renderBoard };
 // })();
-
 
 
 // //logics
